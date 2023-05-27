e5d4096a6613e6bca02ebaa9df518fa5c4c27272
susi1603 <susyremar5@gmail.com>
2023-05-26 20:12:52 -0600

    without develand build files

--- build/aruco_ros/aruco_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/susyreyesm/ros/camera_ws/src/aruco_ros/aruco_msgs
-# Build directory: /home/susyreyesm/ros/camera_ws/build/aruco_ros/aruco_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/aruco_ros/aruco_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/susyreyesm/ros/camera_ws/src/aruco_ros/aruco_ros
-# Build directory: /home/susyreyesm/ros/camera_ws/build/aruco_ros/aruco_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
